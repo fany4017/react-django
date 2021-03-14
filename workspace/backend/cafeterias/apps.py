06957b97eacd211bb35c3710b37c05102a55c1a4
unknown <fany4017@naver.com>
2021-03-14 20:11:14 +0900

    2021-03-14 back & frontend reupload

---
+++ workspace/backend/cafeterias/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CafeteriasConfig(AppConfig):
+    name = 'cafeterias'
---
+++ workspace/backend/cafeterias/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #path('', views.index, name='index'),
+    path('cafeteria/', views.ListMenus.as_view()),
+    path('cafeteria/<slug:site>/<slug:s_date>', views.DetailMenus.as_view()),
+    path('resturant/', views.ListResturant.as_view()),
+
+]
